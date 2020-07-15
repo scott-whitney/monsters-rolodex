234a883dacc4567b1197bb2047ad1b2f67fb1222
Minroe <scott.whitney334@gmail.com>
2020-07-15 14:27:21 -0700

    Updates

---
+++ precache-manifest.5431db3f3436f9ab0f1bb06699fcf149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee9c76786ae9eaa86113b88fd20034a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "efc3671c3b8b642021c4",
+    "url": "/monsters-rolodex/static/css/main.73539f71.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc3671c3b8b642021c4",
+    "url": "/monsters-rolodex/static/js/main.e1048ac0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
